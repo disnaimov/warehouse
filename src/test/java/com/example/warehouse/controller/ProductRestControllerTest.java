b0561df1d93fd196575e7e806bbe5eaed0831832
disnaimov <dimitriy.kiselev0@mail.ru>
2024-07-02 18:57:22 +0400

    Супер Шедулер + фикс теста

--- src/test/java/com/example/warehouse/controller/ProductRestControllerTest.java
+++ src/test/java/com/example/warehouse/controller/ProductRestControllerTest.java
@@ -1,9 +1,11 @@
-/*
     package com.example.warehouse.controller;
 
     import com.example.warehouse.controllers.ProductRestController;
     import com.example.warehouse.dao.ProductRepository;
+    import com.example.warehouse.dto.CreateProductDto;
     import com.example.warehouse.dto.ProductDto;
+    import com.example.warehouse.dto.ProductResponseDto;
+    import com.example.warehouse.dto.UpdateProductDto;
     import com.example.warehouse.service.ProductService;
     import lombok.extern.slf4j.Slf4j;
     import org.junit.jupiter.api.Assertions;
@@ -16,6 +18,7 @@
     import org.springframework.http.HttpStatus;
     import org.springframework.http.ResponseEntity;
 
+    import java.math.BigDecimal;
     import java.sql.Timestamp;
     import java.time.LocalDate;
     import java.util.Date;
@@ -39,13 +42,47 @@
 
             Date date = new Date();
 
+            CreateProductDto createChair = new CreateProductDto();
+            createChair.setName("chair");
+            createChair.setArticle(UUID.randomUUID());
+            createChair.setCategory("category");
+            createChair.setDescription("desc");
+            createChair.setPrice(BigDecimal.valueOf(10));
+            createChair.setQuantity(20);
+
+            UpdateProductDto updateChair = new UpdateProductDto();
+            updateChair.setId(UUID.randomUUID());
+            updateChair.setName("chair");
+            updateChair.setArticle(UUID.randomUUID());
+            updateChair.setCategory("category");
+            updateChair.setDescription("desc");
+            updateChair.setPrice(BigDecimal.valueOf(10));
+            updateChair.setQuantity(20);
+
+            CreateProductDto createTv = new CreateProductDto();
+            createTv.setName("tv");
+            createTv.setArticle(UUID.randomUUID());
+            createTv.setCategory("category2");
+            createTv.setDescription("descccc");
+            createTv.setPrice(BigDecimal.valueOf(100));
+            createTv.setQuantity(2000);
+
+            UpdateProductDto updateTv = new UpdateProductDto();
+            updateTv.setId(UUID.randomUUID());
+            updateTv.setName("tv");
+            updateTv.setArticle(UUID.randomUUID());
+            updateTv.setCategory("category2");
+            updateTv.setDescription("descccc");
+            updateTv.setPrice(BigDecimal.valueOf(100));
+            updateTv.setQuantity(2000);
+
             ProductDto chair = new ProductDto();
             chair.setId(UUID.randomUUID());
             chair.setName("chair");
-            chair.setArticle("1");
+            chair.setArticle(UUID.randomUUID());
             chair.setCategory("category");
             chair.setDescription("desc");
-            chair.setPrice(10);
+            chair.setPrice(BigDecimal.valueOf(10));
             chair.setQuantity(20);
             chair.setCreated(LocalDate.now());
             chair.setLastQuantityUpdate(new Timestamp(date.getTime()));
@@ -53,27 +90,27 @@
             ProductDto tvv = new ProductDto();
             tvv.setId(UUID.randomUUID());
             tvv.setName("tv");
-            tvv.setArticle("2");
+            tvv.setArticle(UUID.randomUUID());
             tvv.setCategory("category2");
             tvv.setDescription("descccc");
-            tvv.setPrice(100);
+            tvv.setPrice(BigDecimal.valueOf(100));
             tvv.setQuantity(2000);
             tvv.setCreated(LocalDate.now());
             tvv.setLastQuantityUpdate(new Timestamp(date.getTime()));
 
-            ResponseEntity<ProductDto> firstCreateResponse = this.productRestController.create(tvv);
-            ResponseEntity<ProductDto> secondCreateResponse = this.productRestController.create(chair);
+            ResponseEntity<UUID> firstCreateResponse = this.productRestController.create(createTv);
+            ResponseEntity<UUID> secondCreateResponse = this.productRestController.create(createChair);
 
-            ResponseEntity<ProductDto> firstGetByIdResponse = this.productRestController.getById(tvv.getId());
-            ResponseEntity<ProductDto> secondGetByIdResponse = this.productRestController.getById(chair.getId());
+            ResponseEntity<ProductResponseDto> firstGetByIdResponse = this.productRestController.getById(tvv.getId());
+            ResponseEntity<ProductResponseDto> secondGetByIdResponse = this.productRestController.getById(chair.getId());
 
-            ResponseEntity<List<ProductDto>> getAllResponse = this.productRestController.getAll(1, 10);
+            ResponseEntity<List<ProductResponseDto>> getAllResponse = this.productRestController.getAll(1, 10);
 
             ResponseEntity<Object> firstRemoveByIdResponse = this.productRestController.deleteById(tvv.getId());
             ResponseEntity<Object> secondRemoveByIdResponse = this.productRestController.deleteById(chair.getId());
 
-            ResponseEntity<ProductDto> firstUpdateResponse = this.productRestController.update(tvv);
-            ResponseEntity<ProductDto> secondUpdateResponse = this.productRestController.update(chair);
+            ResponseEntity<ProductResponseDto> firstUpdateResponse = this.productRestController.update(updateTv);
+            ResponseEntity<ProductResponseDto> secondUpdateResponse = this.productRestController.update(updateChair);
 
             Assertions.assertNotNull(firstCreateResponse);
             Assertions.assertNotNull(secondCreateResponse);
@@ -97,11 +134,10 @@
 
             Assertions.assertEquals(HttpStatus.OK, getAllResponse.getStatusCode());
 
-            Assertions.assertEquals(HttpStatus.OK, firstRemoveByIdResponse.getStatusCode());
-            Assertions.assertEquals(HttpStatus.OK, secondRemoveByIdResponse.getStatusCode());
+            Assertions.assertEquals(HttpStatus.NO_CONTENT, firstRemoveByIdResponse.getStatusCode());
+            Assertions.assertEquals(HttpStatus.NO_CONTENT, secondRemoveByIdResponse.getStatusCode());
 
             Assertions.assertEquals(HttpStatus.OK, firstUpdateResponse.getStatusCode());
             Assertions.assertEquals(HttpStatus.OK, secondUpdateResponse.getStatusCode());
         }
     }
-*/
